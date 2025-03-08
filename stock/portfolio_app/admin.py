0908ae8fd60db72dfde3b7ba61cb4887e3b34058
Arup <arupsarkarbabu@gmail.com>
2025-03-08 18:16:20 +0530

    feature : new model created as basic scaliton
    - model registered in admin
    - gitignore updated to untrack migration files

--- stock/portfolio_app/admin.py
+++ stock/portfolio_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from portfolio_app.models import Stock,Broker,Portfolio
 # Register your models here.
+admin.site.register(Stock)
+admin.site.register(Broker)
+admin.site.register(Portfolio)
--- stock/portfolio_app/models.py
+++ stock/portfolio_app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+# class User(models.Model):
+#     pass
+
+class Stock(models.Model):
+    stock_name = models.CharField(max_length = 200)
+    nse_id = models.CharField(max_length = 500, unique = True)
+    about = models.TextField()
+
+class Broker(models.Model):
+    broker_name = models.CharField(max_length = 200)
+    dmt_id = models.CharField(max_length = 200,unique = True)
+
+class Portfolio(models.Model):
+    # user = 
+    broker = models.ForeignKey(Broker, on_delete = models.CASCADE)
+    # stock_list = ManyToManyField(Stock)
+    stock = models.ForeignKey(Stock, on_delete = models.PROTECT)
+    custom_algo = models.TextField()
+
+
