89ef57d55e7bd9616100c06f317c06893200867b
Arup <arupsarkarbabu@gmail.com>
2025-03-09 15:54:01 +0530

    one portfolio can contain many stocks

--- stock/portfolio_app/models.py
+++ stock/portfolio_app/models.py
@@ -6,17 +6,17 @@ from django.db import models
 class Stock(models.Model):
     stock_name = models.CharField(max_length = 200)
     nse_id = models.CharField(max_length = 500, unique = True)
-    about = models.TextField()
+    about = models.JSONField()
+
 
 class Broker(models.Model):
     broker_name = models.CharField(max_length = 200)
     dmt_id = models.CharField(max_length = 200,unique = True)
 
 class Portfolio(models.Model):
-    # user = 
+    # user =  models.ForeignKey(USER, on_delete = models.PROTECT)
     broker = models.ForeignKey(Broker, on_delete = models.CASCADE)
-    # stock_list = ManyToManyField(Stock)
-    stock = models.ForeignKey(Stock, on_delete = models.PROTECT)
+    stock_list = models.ManyToManyField(Stock)
     custom_algo = models.TextField()
 
 
