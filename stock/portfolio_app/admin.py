c7702504aeb0736d9f57e9fec98aa8c96286d76f
Arup <arupsarkarbabu@gmail.com>
2025-03-09 19:08:33 +0530

    `Added StockOrder model and updated admin.py to register new models`

--- stock/portfolio_app/admin.py
+++ stock/portfolio_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from portfolio_app.models import Stock,Broker,Portfolio
+from portfolio_app.models import Stock,Broker,Portfolio,StockOrder
 # Register your models here.
 admin.site.register(Stock)
 admin.site.register(Broker)
 admin.site.register(Portfolio)
+admin.site.register(StockOrder)
--- stock/portfolio_app/models.py
+++ stock/portfolio_app/models.py
@@ -1,23 +1,56 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
-# class User(models.Model):
-#     pass
+#---validator--------------
+def validate_id(value):
+    if len(value) > 3:
+        return True
+    else:
+        # raise Exception("bad") # tool broken
+        raise ValidationError('bad')
+
+def stock_count(value):
+    # validation not working
+    return True if value>=1 else False 
+#--------------------------
+
 
 class Stock(models.Model):
-    stock_name = models.CharField(max_length = 200)
+    name = models.CharField(max_length = 200)
     nse_id = models.CharField(max_length = 500, unique = True)
-    count =  models.IntegerField(default = 0)
     about = models.JSONField()
 
+    def __str__(self):
+        return self.name 
 
 class Broker(models.Model):
-    broker_name = models.CharField(max_length = 200)
-    dmt_id = models.CharField(max_length = 200,unique = True)
+    name = models.CharField(max_length = 200)
+    dmt_id = models.CharField(
+        max_length = 200,
+        unique = True,
+        validators = [validate_id]
+        )
+    
+    def __str__(self):
+        return self.name
+
+
 
 class Portfolio(models.Model):
+    name = models.CharField(max_length = 100,default= "NULL")
     # user =  models.ForeignKey(USER, on_delete = models.PROTECT)
     broker = models.ForeignKey(Broker, on_delete = models.CASCADE)
     stock_list = models.ManyToManyField(Stock)
     custom_algo = models.TextField()
 
+    def __str__(self):
+        return self.name
 
+
+class StockOrder(models.Model):
+    portfolio = models.ForeignKey(Portfolio , on_delete = models.CASCADE)
+    stock = models.ForeignKey(Stock , on_delete = models.CASCADE)
+    count =  models.IntegerField(default = 0, validators = [stock_count])
+    # not able to identify is stock present in portfolio or not
+    def __str__(self):
+        return f"order:{self.portfolio.name}-{self.stock.name}-{self.count}"
