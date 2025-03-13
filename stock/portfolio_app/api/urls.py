8fd22bfae78924458433a8683edd451c772596b8
Arup <arupsarkarbabu@gmail.com>
2025-03-13 14:12:12 +0530

    serializer data visible in browzers
    - views is modified
    - new urls added to get all data

--- stock/portfolio_app/api/urls.py
+++ stock/portfolio_app/api/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from portfolio_app.api.views import stock_data
+from portfolio_app.api.views import stock_data,broker_data,portfolio_data
 
 urlpatterns = [
     path("stocks/",stock_data),
-    path("stocks/<str:name>",stock_data)
+    path("stocks/<str:name>",stock_data),
+    path("brokers/",broker_data),
+    path("brokers/<str:name>",broker_data),
+    path("Portfolios/",portfolio_data),
+    path("Portfolios/<str:name>",portfolio_data)
 ]
--- stock/portfolio_app/api/views.py
+++ stock/portfolio_app/api/views.py
@@ -3,17 +3,50 @@ from rest_framework.response import Response
 
 from portfolio_app.models import Stock,Broker,Portfolio,StockOrder
 from portfolio_app.api.serializers import StockSerial, BrokerSerial, PortfolioSerial#, StockOrderSerial
-
-
+#----------------utility------------------------------------------
+def get_json_data(
+        model,
+        serializer,
+        name = None,
+        custom_query = None
+    ):
+    '''the function will convert database response in to json/dict response'''
+    # get query
+    if name:
+        query = model.objects.filter(name=name)
+    elif custom_query:
+        try:
+            query = custom_query
+        except:
+            print("query is not correct")
+    else:
+        query = model.objects.all()
+    # serialize query
+    serial = serializer(query,many = True)
+    # return json response
+    return serial.data
+#--------------------------------------------------------------------
 
 @api_view(["GET"])
 def stock_data(request, name = None):
-    # Get the stock data from the database
-    stocks = None
-    if not name:
-        stocks = Stock.objects.all()
-    else:
-        stocks = Stock.objects.filter(name = name)
+    '''Get the stock data from the database'''
+    return Response(
+        get_json_data(Stock,StockSerial,name), 
+        status=200
+    )
 
-    serializer = StockSerial(stocks ,many = True)
-    return Response(serializer.data , status=200)
+
+@api_view(["GET"])
+def broker_data(request, name = None):
+    '''Get the stock data from the database'''
+    return Response(
+         get_json_data(Broker,BrokerSerial,name),
+        status=200
+    )
+
+@api_view(["GET"])
+def portfolio_data(request, name = None):
+    return Response(
+         get_json_data(Portfolio,PortfolioSerial,name),
+        status=200
+    )
